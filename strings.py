be5aeb2679f2a73c92b2e5bb29fe78725b100b16
Kothapelli Kiranmai <116304133+kiranmai17@users.noreply.github.com>
2022-11-07 22:27:17 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,15 @@
+"""
+DAY:monday
+DATE:7th nov 2022
+TOPIC:strings
+AUTHOR:kiranmai
+"""
+b=""
+print(b.isspace())    #false
+b="   "
+print(b.isspace())     #true
+b="8"
+print(b.isspace())   #false
+a=input()    #given value 7
+b=input()   #given value 8
+print(a+b)   #78
